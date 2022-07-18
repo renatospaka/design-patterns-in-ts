02b78814cb33e41158a006d8e080bd065fc768ec
Renato Spakauskas <renatospaka@gmail.com>
2022-07-18 11:29:08 -0300

    test: :white_check_mark: testing customer
    add test coverage to the customer entity

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,10 +1,10 @@
 import Address from "./address";
 
 export default class Customer {
-  _id: string;
-  _name: string;
-  _address!: Address;
-  _active: boolean = false;
+  private _id: string;
+  private _name: string;
+  private _address!: Address;
+  private _active: boolean = false;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -13,13 +13,17 @@ export default class Customer {
     this.validate();
   }
 
+  get name(): string {
+    return this._name;
+  }
+
   changeName(name: string) {
     this._name = name;
   }
 
   activate() {
     if (this._address === undefined) {
-      throw new Error("Address is mandatory to activate customer");      
+      throw new Error("address is mandatory to activate customer");      
     }
     this._active = true;
   }
@@ -28,13 +32,17 @@ export default class Customer {
     this._active = false;
   }
 
+  isActive(): boolean {
+    return this._active;
+  }
+
   validate() {
     this._active = false
-    if (this._name.length === 0) {
-      throw new Error("Name is required");      
-    }
     if (this._id.length === 0) {
-      throw new Error("Id is required");
+      throw new Error("id is required");
+    }
+    if (this._name.length === 0) {
+      throw new Error("name is required");      
     }
   }
 
