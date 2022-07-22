98137475a4d280979d824dd8a926c2929151ea46
Renato Spakauskas <renatospaka@gmail.com>
2022-07-22 12:24:18 -0300

    test: :wrench: using jest with tsc
    using jest with tsc (-- --noEmit) to forces ts to evaluate all functions and methods signatures before executing tests

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rua Dois", 2, "09876-543", "Sampa");
 customer.Address(address);
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 20);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 1);
+const item2 = new OrderItem("2", "Item 2", 20, "p2", 1);
 const order = new Order("1", "123", [item1, item2]);
