0a1fce4b7a05b75af0a448dc076cba4e343077be
Renato Spakauskas <renatospaka@gmail.com>
2022-07-25 14:36:25 -0300

    feat: :construction: new events for product
    notify method of the product event

--- src/domain/event/@shared/eventDispatcher.spec.ts
+++ src/domain/event/@shared/eventDispatcher.spec.ts
@@ -1,5 +1,6 @@
 import EventDispatcher from "./eventDispatcher";
 import SendEmailWhenProductIsCreatedHandler from "./product/handler/sendEmailWhenProductIsCreated.handler";
+import ProductCreatedEvent from "./product/productCreatedEvent";
 
 describe("Domain events tests", () => {
   it("should register an event handler", () => {
@@ -27,11 +28,30 @@ describe("Domain events tests", () => {
   it("should register all event handlers", () => {
     const eventDispatcher = new EventDispatcher();   
     const eventHandler = new SendEmailWhenProductIsCreatedHandler();  
-
+    
     eventDispatcher.register("ProductCreatedEvent", eventHandler);
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
     
     eventDispatcher.unregisterAll();
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBe(undefined);
   });
+
+  it("should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();   
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler(); 
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "Product 1",
+      description: "Description",
+      price: 10.0,
+    });
+
+    // quando o notify for executado, o SendEmailWhenProductIsCreatedHandler.handle() deve ser executado
+    eventDispatcher.notify(productCreatedEvent);
+    expect(spyEventHandler).toHaveBeenCalled();
+  })
 });
--- src/domain/event/@shared/eventDispatcher.ts
+++ src/domain/event/@shared/eventDispatcher.ts
@@ -9,7 +9,14 @@ export default class EventDispatcher implements EventDispatcherInterface {
     return this.eventHandlers;
   }
 
-  notify(event: EventInterface): void {}
+  notify(event: EventInterface): void {
+    const eventName = event.constructor.name;
+    if (this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName].forEach((eventHandler) => {
+        eventHandler.handle(event);
+      });
+    }
+  }
 
   register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
     if (!this.eventHandlers[eventName]) {
