55acb84b558a752f5a2d8d6a72a8c1c84d5d870e
Renato Spakauskas <renatospaka@gmail.com>
2022-07-28 10:57:57 -0300

    feat: :sparkles: rewardsPoints and addRewardPoints for customer entity
    adding rewardsPoints and addRewardPoints methots to the customer entity

--- src/domain/entity/customer.spec.ts
+++ src/domain/entity/customer.spec.ts
@@ -47,4 +47,15 @@ describe("Customer unit tests", () => {
       customer.activate();
     }).toThrowError("address is mandatory to activate customer")
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
