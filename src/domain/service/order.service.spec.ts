dde1c564335b4900706654c17d5ad572ab833e9d
Renato Spakauskas <renatospaka@gmail.com>
2022-07-28 10:59:19 -0300

    feat: :sparkles: new domain service for order
    created placeOrder method

--- src/domain/service/order.service.spec.ts
+++ src/domain/service/order.service.spec.ts
@@ -1,3 +1,4 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
@@ -12,4 +13,13 @@ describe("Order service unit tests", () => {
     const total = OrderService.total([order1, order2]);
     expect(total).toBe(500);
   });
+
+  it("should place an order", () => {
+    const customer = new Customer("c1", "Customer1");
+    const item1 = new OrderItem("i1", "Item 1", 10, "product 1", 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  })
 });
