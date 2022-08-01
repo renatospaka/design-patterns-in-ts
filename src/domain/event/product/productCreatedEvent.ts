7d91b18f98dc33d1dd1037433d25d1d1330c1af1
Renato Spakauskas <renatospaka@gmail.com>
2022-08-01 10:01:30 -0300

    style: :truck: product domain events
    spliting structure from @shared folder

--- src/domain/event/product/productCreatedEvent.ts
+++ src/domain/event/product/productCreatedEvent.ts
@@ -1,4 +1,4 @@
-import EventInterface from "../event.interface";
+import EventInterface from "../@shared/event.interface";
 
 export default class ProductCreatedEvent implements EventInterface {
   dataTimeOccurred: Date;
