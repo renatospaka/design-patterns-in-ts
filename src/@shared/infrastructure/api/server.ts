40b5b9376e0ed6231004d78978ef4394ad1fce45
Renato Spakauskas <renatospaka@gmail.com>
2022-11-22 21:09:41 -0300

    fix: :wrench: application server
    setting up .env  configuration file

--- src/@shared/infrastructure/api/server.ts
+++ src/@shared/infrastructure/api/server.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { app } from "./express";
 
 dotenv.config();
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(process.env.PORT);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
